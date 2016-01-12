51644792014e0ececf3dcc31ddb02fbd72f6d441
Single <musicman_leehom@126.com>
2016-01-12 14:35:16 +0800

    Initial Commit

---
+++ RAC_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAC_Demo
+//
+//  Created by Single on 16/1/12.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAC_Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RAC_Demo
+//
+//  Created by Single on 16/1/12.
+//  Copyright © 2016年 single. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
